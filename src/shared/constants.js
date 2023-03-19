3355bab3c28311ee94d811e26cdd1c2ba1d786cd
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2023-03-19 01:15:53 +0100

    Add proper extension to file

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,15 +1,17 @@
 module.exports = Object.freeze({
     PLAYER_RADIUS: 20,
-    PLAYER_SPEED: 400,
+    PLAYER_SPEED: 50,
 
     SCORE_PER_FOOD: 1,
 
-    MAP_SIZE: 5000,
+    MAP_SIZE: 100,
 
     MSG_TYPES: {
         JOIN_GAME: 'join_game',
         GAME_UPDATE: 'update',
         INPUT: 'input',
         GAME_OVER: 'dead',
-    }
+    },
+
+    SERVER_UPDATE_FREQ: 1000 / 60
 });
